bb1bbfcf79b822db96afe049a92c30a7b1bd0074
coriagogo <pkcparis@gmail.com>
2019-05-03 12:18:27 -0400

    cards displaying

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{props.headline}</div>
+      <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'props.img'} alt={props.headline} />
+          <img src={props.card.img} alt={props.card.headline} />
         </div>
-        <span>By {props.author}</span>
+        <span>By {props.card.author}</span>
       </div>
     </div>
   );
--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -5,8 +5,8 @@ import Card from './Card';
 const Cards = props => {
   return (
     <div className="cards-container">
-      {props.cards.map(item => {
-        return <Card card={item} img={item.img} author={item.author} tab={item.tab} headline={item.headline} key={item.headline} />
+      {props.cards.map((card, index) => {
+        return <Card card={card} key={index} />
       })};
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/ }
@@ -15,7 +15,7 @@ const Cards = props => {
 };
 
 Cards.propTypes = {
-  Cards:PropTypes.array,
+  cards:PropTypes.array,
 }
 
 // Make sure you include prop types for all of your incoming props
