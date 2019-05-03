66d1ce1744ca0341d5e47bbe3c281f0421528467
coriagogo <pkcparis@gmail.com>
2019-05-03 13:15:19 -0400

    all styled components added

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,6 +1,72 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { carouselData } from '../../data'
 // Complete this Carousel 
+
+const CarouselStyles = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  height: 500px;
+  position: relative;
+  overflow: hidden;
+  margin-top: 16px;
+
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`
+
+const LeftButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  font-size: 40px;
+  border-radius: 50%;
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  top: 50%;
+  left: 25px;
+  transform: translate(0, -50%);
+
+  &:hover {
+    color: #333;
+    background-color: #fff;
+    border: 2px solid #333;
+  }
+`
+
+const RightButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  font-size: 40px;
+  border-radius: 50%;
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  top: 50%;
+  right: 25px;
+  transform: translate(0, -50%);
+
+  &:hover {
+    color: #333;
+    background-color: #fff;
+    border: 2px solid #333;
+  }
+`
+
 export default class Carousel extends Component {
   constructor(props){
     super(props);
@@ -26,10 +92,10 @@ export default class Carousel extends Component {
   
   render(){
     return (
-      <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
-      </div>
+      <CarouselStyles>
+        <LeftButton onClick={this.leftClick}>{"<"}</LeftButton>
+        <RightButton onClick={this.rightClick}>{">"}</RightButton>
+      </CarouselStyles>
     )
   }
 }
